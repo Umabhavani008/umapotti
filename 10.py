3449511826a09ff2fdc6d42e393d46159d8bbbde
Umabhavani008 <131332830+Umabhavani008@users.noreply.github.com>
2023-05-11 10:49:33 +0530

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,8 @@
+hello world and practice makes perfect and hello world again
+Then, the output should be:
+again and hello makes perfect practice world
+
+
+word = sorted(list(set(input().split())))              
+                                                       
+print(" ".join(word))
